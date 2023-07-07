1d350a98ff86805a7b046b87ca663660087ad3f3
Sami Islam <islam.samipkr@gmail.com>
2023-07-07 09:45:43 -0400

    added

--- main.tf
+++ main.tf
@@ -8,8 +8,26 @@
   project               = "sami-islam-project101-dev"
   target                = "https://www.googleapis.com/compute/beta/projects/sami-islam-project101-dev/global/targetHttpsProxies/samiloadbalancer-target-proxy"
 }*/
+resource "google_compute_region_network_endpoint_group" "defaultneg1" {
+  name                  = "defaultneg1"
+  network_endpoint_type = "SERVERLESS"
+  region                = "us-central1"
+  cloud_function {
+    function = "function1"
+  }
+  project = var.project_id
+}
+resource "google_compute_region_network_endpoint_group" "defaultneg2" {
+  name                  = "defaultneg2"
+  network_endpoint_type = "SERVERLESS"
+  region                = "northamerica-northeast1"
+  cloud_function {
+    function = "function1"
+  }
+  project = var.project_id
+}
 resource "google_compute_region_network_endpoint_group" "negfetchdata1" {
-  name                  = "negfetchdata1"
+  name                  = "defaultneg2"
   network_endpoint_type = "SERVERLESS"
   region                = "us-central1"
   project = var.project_id
@@ -43,6 +61,24 @@ resource "google_compute_region_network_endpoint_group" "negupdatedata2" {
     function = "function1"
   }
   project = var.project_id
+}
+resource "google_compute_backend_service" "defaultbackend" {
+  connection_draining_timeout_sec = 0
+  load_balancing_scheme           = "EXTERNAL_MANAGED"
+  locality_lb_policy              = "ROUND_ROBIN"
+  name                            = "defaultbackend"
+  port_name                       = "http"
+  project                         = "sami-islam-project101-dev"
+  protocol                        = "HTTPS"
+  session_affinity                = "NONE"
+  timeout_sec                     = 30
+    backend {
+    group = google_compute_region_network_endpoint_group.defaultneg1.self_link
+  }
+
+  backend {
+    group = google_compute_region_network_endpoint_group.defaultneg2.self_link
+  }
 }
 resource "google_compute_backend_service" "backendfetchdata" {
   connection_draining_timeout_sec = 0
@@ -84,19 +120,19 @@ resource "google_compute_url_map" "serverlesshttploadbalancer" {
   default_service = google_compute_backend_service.backendfetchdata.self_link
   host_rule {
     hosts        = ["srv.demoapp1.web.ca"]
-    path_matcher = "path-matcher-fetchdata"
+    path_matcher = "path-matcher"
   }
 
   host_rule {
     hosts        = ["srv.demoapp1.web.ca"]
-    path_matcher = "path-matcher-fetchdata"
+    path_matcher = "path-matcher"
   }
 
   name = "serverlesshttploadbalancer"
 
   path_matcher {
-    default_service = google_compute_backend_service.backendfetchdata.self_link
-    name            = "path-matcher-fetchdata"
+    default_service = google_compute_backend_service.defaultbackend.self_link
+    name            = "path-matcher"
 
     path_rule {
       paths   = ["/fetchdata"]
