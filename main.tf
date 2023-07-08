5a39b33b78315473d8d0c0bd6cdf9e6f63fbbc8e
Sami Islam <islam.samipkr@gmail.com>
2023-07-08 17:36:53 -0400

    happy ending

--- main.tf
+++ main.tf
@@ -1,4 +1,76 @@
-/*resource "google_compute_global_forwarding_rule" "testport" {
+
+locals{
+    cloud_functions_file_list = [for f in fileset("${path.module}/loadbalancerconfig", "[^_]*.yaml") : yamldecode(file("${path.module}/loadbalancerconfig/${f}"))]
+
+}
+    cloud_functions_list=flatten([
+      for cloud_function in local.cloud_functions_file_list:[
+        for function in try(cloud_function.mobility_cloud_functions_list,[]):{
+          name="$neg${function}${var.region}"
+        }
+      ]
+    ])
+
+
+module "neg_northamerica_northeast1" {
+  source = "./modules/networkendpointgroup"
+  for_each={for function in local.cloud_functions_list: var.region => function}
+  project=var.project_id
+  region        = each.region
+  function_name = each.value.function
+
+}
+/*
+locals {
+  cloud_armor_policies = [for f in fileset("${path.module}/cloudarmorconfigs", "[^_]*.yaml") : yamldecode(file("${path.module}/cloudarmorconfigs/${f}"))]
+  cloud_armor_list = flatten([
+    for cloud_armor_policy in local.cloud_armor_policies : [
+      for policy in try(cloud_armor_policy.central_policy, []) : {
+        name               = policy.name
+        project_id         = policy.project_id
+        description        = try(policy.description, [])
+        default_rule_action= try(policy.default_rule_action, [])  
+        type               = try(policy.type, [])
+        json_parsing       = try(policy.json_parsing, [])
+        layer_7_ddos_defense_enable = try(policy.layer_7_ddos_defense_enable, [])
+        layer_7_ddos_defense_rule_visibility = try(policy.layer_7_ddos_defense_rule_visibility, [])
+        default_rule_action          = try(policy.default_rule_action,[])
+        pre_configured_rules =try(policy.pre_configured_rules,[])
+      }
+    ]
+  ])
+}
+module "cloud_armor" {
+  source = "./modules/cloud-armor"
+  for_each     = { for policy in local.cloud_armor_list : "${policy.name}-${policy.project_id}" => policy }
+  project_id = each.value.project_id
+  name = each.value.name
+  description = each.value.description
+  json_parsing = each.value.json_parsing #"STANDARD"
+  #Enable Adaptive Protection
+  layer_7_ddos_defense_enable = each.value.layer_7_ddos_defense_enable #true
+  layer_7_ddos_defense_rule_visibility = each.value.layer_7_ddos_defense_rule_visibility #"STANDARD"
+
+  default_rule_action          = each.value.default_rule_action #"deny(404)"
+  #Add pre-configured rules
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+resource "google_compute_global_forwarding_rule" "testport" {
   ip_address            = "34.117.32.104"
   ip_protocol           = "TCP"
   ip_version            = "IPV4"
@@ -7,7 +79,7 @@
   port_range            = "443-443"
   project               = "sami-islam-project101-dev"
   target                = "https://www.googleapis.com/compute/beta/projects/sami-islam-project101-dev/global/targetHttpsProxies/samiloadbalancer-target-proxy"
-}*/
+}
 resource "google_compute_region_network_endpoint_group" "defaultneg1" {
   name                  = "defaultneg1"
   network_endpoint_type = "SERVERLESS"
@@ -116,7 +188,10 @@ resource "google_compute_backend_service" "backendupdatedata" {
     group = google_compute_region_network_endpoint_group.negupdatedata2.self_link
   }
 }
-resource "google_compute_url_map" "serverlesshttploadbalancer" {
+resource "google_compute_url_map" "serverlesshttploadbalancerfrontend" {
+  frontend {
+    name = "testport"
+  }
   default_service = google_compute_backend_service.backendfetchdata.self_link
   host_rule {
     hosts        = ["srv.demoapp1.web.ca"]
