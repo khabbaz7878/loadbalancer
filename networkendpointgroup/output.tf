ba1bd86fbbc80b19a6c9d5fb6dd3dcdeee883fcf
Sami Islam <islam.samipkr@gmail.com>
2023-07-08 22:08:56 -0400

    neg created

--- networkendpointgroup/output.tf
+++ networkendpointgroup/output.tf
@@ -1,7 +1,12 @@
-output "neg_name" {
+output "neg_name_north_america" {
     value = google_compute_region_network_endpoint_group.neg["northamerica-northeast1"]
 }
-
-output "neg_id" {
+output "neg_name_us_central" {
+  value = google_compute_region_network_endpoint_group.neg["us-central1"]
+}
+output "neg_id_north_america" {
     value = google_compute_region_network_endpoint_group.neg["us-central1"]
+}
+output "neg_id_us_central" {
+    value = google_compute_region_network_endpoint_group.neg["northamerica-northeast1"]
 }
