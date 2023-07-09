cea19d6a1604cedae3ec0312c5f1d7a86ca55715
Sami Islam <islam.samipkr@gmail.com>
2023-07-08 22:16:47 -0400

    neg_finalized

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "neg" {
   source = "./networkendpointgroup"
   region        = ["northamerica-northeast1","us-central1"]
   for_each={for region,function in local.cloud_functions_list: region => function}
-  name=each.value.neg_name
+  name="neg-${each.value.neg_name}"
   project_id=var.project_id
 
   function_name = each.value.name
