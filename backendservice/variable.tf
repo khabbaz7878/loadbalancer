4a21b416a7592b094158e67c5acb5582df308251
Sami Islam <islam.samipkr@gmail.com>
2023-07-09 00:29:46 -0400

    hi

---
+++ backendservice/variable.tf
@@ -0,0 +1,28 @@
+variable "name"{
+
+}
+variable "project_id" {
+
+  
+}
+variable "backend" {
+  
+}
+variable "load_balancing_scheme" {
+  
+}
+variable "port_name" {
+  
+}
+variable "protocol" {
+  
+}
+variable "session_affinity" {
+  
+}
+variable "timeout_sec" {
+  
+}
+variable "backend" {
+  
+}
