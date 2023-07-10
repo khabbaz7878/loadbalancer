8b608d0508abe5b05cd1f5258fb26c27856948cd
Sami Islam <islam.samipkr@gmail.com>
2023-07-10 01:32:42 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -74,8 +74,7 @@ resource "google_compute_target_https_proxy" "default" {
   project = var.project_id
   name    = "mobility-https-proxy"
   url_map = google_compute_url_map.serverlesshttploadbalancerfrontend.self_link
-  ssl_certificates=var.managed_ssl_certificate_domains
-  certificate_map=google_compute_managed_ssl_certificate.default.self_link
+  ssl_certificates=[google_compute_managed_ssl_certificate.default.self_link]
 }
 resource "google_compute_global_forwarding_rule" "serverlesshttploadbalancerfrontend" {
   ip_protocol           = "TCP"
